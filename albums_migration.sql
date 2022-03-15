42601d901bfbdf426ae780f55a9faaadd27ce1b0
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-15 14:34:53 -0500

    added table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,24 @@
-USE codeup_test_db;
+USE
+codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+-- TODO Create an albums table with the following columns:
+--
+-- id — auto-incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
+
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50),
+    name         VARCHAR(100) NOT NULL,
+    release_date INT UNSIGNED,
+    sales        FLOAT,
+    genre        VARCHAR(50),
+    PRIMARY KEY (id)
+);
